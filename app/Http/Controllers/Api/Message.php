a37bbe91374e5b1b1243d0e68f3442a1c368cae9
景肖然 <jingxiaoran@99fenqi.com>
2016-09-29 00:02:16 +0800

    Api/Comment.php

--- app/Http/Controllers/Api/Message.php
+++ app/Http/Controllers/Api/Message.php
@@ -74,6 +74,7 @@ class Message extends Controller
         }
         $order->my_name = $user->name;
         $order->my_headimgurl = $user->headimgurl;
+        $order->with_user_id = $one->id;
         $order->with_user_name = $one->name;
         $order->with_user_headimgurl = $one->headimgurl;
         $order->with_user_follows = FollowM::where('user_id',$user->id)->count('id');
